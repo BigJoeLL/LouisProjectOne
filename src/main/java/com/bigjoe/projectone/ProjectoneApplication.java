99a659eee0ca4fc7b5463d7d60c6c78597e6c178
BigJoeLL <80508496+BigJoeLL@users.noreply.github.com>
2022-04-01 18:19:03 +0800

    INIT

---
+++ src/main/java/com/bigjoe/projectone/ProjectoneApplication.java
@@ -0,0 +1,13 @@
+package com.bigjoe.projectone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectoneApplication.class, args);
+	}
+
+}
