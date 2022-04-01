6f2e75ba9b93a26182a3221e5840840a38c7c35f
BigJoeLL <80508496+BigJoeLL@users.noreply.github.com>
2022-04-01 20:46:33 +0800

    新增Mock接口

---
+++ app/model/src/main/java/com/louis/projectone/model/request/ProjectOneMockRequest.java
@@ -0,0 +1,9 @@
+package com.louis.projectone.model.request;
+
+/**
+ * LouisProjectOne
+ * com.louis.projectone.model.request
+ * Created by luyib on 2022/4/1 19:03
+ */
+public class ProjectOneMockRequest {
+}
---
+++ app/model/src/main/java/com/louis/projectone/model/response/ProjectOneMockResponse.java
@@ -0,0 +1,9 @@
+package com.louis.projectone.model.response;
+
+/**
+ * LouisProjectOne
+ * com.louis.projectone.model.response
+ * Created by luyib on 2022/4/1 19:03
+ */
+public class ProjectOneMockResponse {
+}
